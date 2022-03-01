e33ce034319f845491ac93133854d45dca309627
Michelle <mhedvard@gmail.com>
2022-03-01 21:26:10 +0100

    did homework vec part A

---
+++ homework/Vec/A/main.cs
@@ -0,0 +1,29 @@
+using System;
+using static System.Console;
+using static System.Math;
+using static vec;
+
+class main{
+	static void Main(){
+		// Vectors and constant used for test
+		vec vecA =new vec(1, 2, 3);
+		vec vecB =new vec(4, 5, 6);
+		double a = 2.0;
+		
+		WriteLine("Vectors and constants used for tests");
+		vecA.print("vecA =");
+		vecB.print("vecB =");
+		WriteLine($"a = {a}");
+		WriteLine(" ");
+
+		// Test of operatores
+		WriteLine("Test of operators part A");
+		var vecC  = a*vecA;
+		(vecC).print("a*VecA = ","(should be equal 2 4 6)");
+		
+		(vecA+vecB).print("VecA + VecB = ","(should be equal 5 7 9)");
+		(vecA-vecB).print("VecA - VecB = ","(should be equal -3 -3 -3)");
+		(-vecA).print("-VecA = ","(should be equal -1 -2 -3)");
+		
+	}
+}
---
+++ homework/Vec/A/vec.cs
@@ -0,0 +1,44 @@
+using static System.Console;
+
+
+public class vec{
+	public double x,y,z;
+
+	//constructors:
+	public vec(){ x=y=z=0; }
+	public vec(double a,double b,double c){ 
+		x=a;
+		y=b;
+		z=c; 
+	}
+	
+	//operators:
+	public static vec operator*(vec v, double c){
+		return new vec(c*v.x,c*v.y,c*v.z);
+	}
+	public static vec operator*(double c, vec v){return v*c;}
+	public static vec operator+(vec u, vec v){
+		return new vec(u.x+v.x, u.y+v.y, u.z+v.z);
+	}
+	public static vec operator-(vec u, vec v){
+		return new vec(u.x-v.x, u.y-v.y, u.z-v.z);
+	}
+	public static vec operator-(vec u){
+		return new vec(-u.x, -u.y, -u.z);
+	}
+	
+	//methods:
+	public void print(string s){
+		Write(s);
+		WriteLine($"{x} {y} {z}");
+	}
+
+	public void print(string s, string t){
+		Write(s);
+		Write($" {x} {y} {z} ");
+		WriteLine(t);
+	}
+	public void print(){
+		this.print("");
+	}
+}
