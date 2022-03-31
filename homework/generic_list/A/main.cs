d176b4cac26c1df25856b9b7cfdb37c640949ff7
Michelle <mhedvard@gmail.com>
2022-03-31 12:02:08 +0200

    homework generic part A

---
+++ homework/generic_list/A/main.cs
@@ -0,0 +1,26 @@
+using System; 
+using static System.Console;
+
+class main{
+	static public void Main(){
+		var list = new genlist <double[]>(); 
+		char[] delimiters = {' ','\t'};
+		var options = StringSplitOptions.RemoveEmptyEntries; 
+		
+		for(string line = ReadLine(); line!=null; line=ReadLine()){
+			var words = line.Split(delimiters,options); 
+			int n = words.Length;
+			var numbers = new double[n]; 
+			for(int i=0; i<n; i++) 
+				numbers[i] = double.Parse(words[i]); 
+			list.push(numbers); 
+		}
+
+		for(int i=0; i<list.size;i++){
+			var numbers = list.data[i];
+			foreach(var number in numbers)
+				Write($"{number:e}");
+			WriteLine();
+		}
+	}
+}
