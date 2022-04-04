8d180c4bd82ca4c4c547bf4c23bea0c4ad59f289
Michelle <mhedvard@gmail.com>
2022-04-04 21:48:14 +0200

    did plot but part C is not done

---
+++ homework/plots/C/complexG.cs
@@ -0,0 +1,29 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+static class main{
+	static double complexG(double x){
+		///single precision gamma function (Gergo Nemes, from Wikipedia)
+		if(x<0)	
+			return PI/Sin(PI*x)/gamma(1-x);
+		if(x<9)
+			return gamma(x+1)/x;
+	
+		double lngamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+		return Exp(lngamma);
+	}	
+
+	static double lngamma(double x){
+		if(x<=0) // Not defined for x<=0
+			return 0.0; 
+		double lngamma = (x-1/2)*Log(x)-x+1/2*Log(2*PI);
+		return lngamma;
+	}
+ 
+
+	static void  Main(){
+		for(double x =-5; x<=20; x+=0.01)
+			WriteLine($"{x}	{gamma(x)}	{lngamma(x)}");
+	}
+}
