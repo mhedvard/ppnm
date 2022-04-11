3201870becf88d8509ec43ac1644d3b0218dccaf
Michelle <mhedvard@gmail.com>
2022-04-11 15:51:54 +0200

    working on splies part A

--- homework/splines/A/main.cs
+++ homework/splines/A/main.cs
@@ -9,6 +9,11 @@ class main{
 		double[] x = {1,3,5,7,11,25,30}; 
 		double[] y = {1,5,7,4,-5,-8,3};
 		
+		for(int i = 0; i < x.Length; i++)
+			WriteLine($"{x[i]}	{y[i]}"); 
+		WriteLine("");
+		WriteLine("");		
+
 		for(double xp = x[0]; xp <= x[x.Length-1]; xp+=0.01){
 			WriteLine($"{xp}	{linterpPoint(x,y,xp)}	{linterpInteg(x,y,xp)}");
 		}
