556afeeb669774f8e6ef3a00cb4a4a49b762da25
Michelle <mhedvard@gmail.com>
2022-04-12 10:45:16 +0200

    Working on spline part B

--- homework/splines/B/main.cs
+++ homework/splines/B/main.cs
@@ -7,9 +7,14 @@ class main{
 		double[] x = {0,1,2,3,4,5,6}; 
 		double[] y = {0,1,2,0,-2,-1,0};
 
+		for(int i = 0; i < x.Length; i++)
+			WriteLine($"{x[i]}	{y[i]}"); 
+		WriteLine("");
+		WriteLine("");
+
 		var qs = new qspline(x,y);
-		for(double z = x[0]; z<= x[x.Length-1]; z+=0.01)
-			WriteLine($" {x}	{qs.spline(z)}");
+		for(double z = x[0]; z<= x[x.Length-1]; z+=0.1)
+			WriteLine($" {z}	{qs.spline(z)}");
 		/*
 		for(int i = 0; i < x.Length; i++)
 			WriteLine($"{x[i]}	{y[i]}"); 
