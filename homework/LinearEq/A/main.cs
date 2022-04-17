e3d6a9a9aba8e840727199a26223d7994ae0e685
Michelle <mhedvard@gmail.com>
2022-04-17 14:11:16 +0200

    did LinearEq part A

---
+++ homework/LinearEq/A/main.cs
@@ -0,0 +1,60 @@
+using System; 
+using static System.Console; 
+using static System.Math; 
+//using static matrix;
+//using static vector;  
+
+class main{
+	static void Main(){
+		int n=7,m=7; 
+		WriteLine("Generate matrix A and vector b.");
+		matrix A = new matrix(n,m);
+		vector b = new vector(n); 
+
+		Random rnd = new Random(); 
+		
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				A[i,j]=rnd.Next(10);
+			}
+		}
+		for(int i=0; i<n; i++)
+			b[i] = rnd.Next(10);
+
+		WriteLine("Matrix A:");
+		A.print();
+		WriteLine("Vector B:");
+		b.print();
+
+		WriteLine("Factorize A into QR.");
+		QRGS qrgs = new QRGS(A);
+		matrix R = qrgs.R;
+		matrix Q = qrgs.Q;
+		WriteLine("Right triangular matris R:");
+		R.print(); 
+		WriteLine("Orthogonal matrix Q:");
+		Q.print();
+		WriteLine("Test that Q^T*Q =I:");
+		var QTQ = Q.T*Q;
+		QTQ.print();
+
+		WriteLine("Test that QR - A = [0]:");
+		var QRA = Q*R-A;
+		QRA.print();
+
+		
+		WriteLine("Solve QRx = b.");
+		var x = qrgs.solve(b);
+		WriteLine("Result for x:");
+		x.print();
+
+		WriteLine("Check Ax = b.");
+		var Ax = A*x;
+		WriteLine("Result for Ax:");
+		Ax.print(); 
+		WriteLine("Vextor b:");
+		b.print();
+ 
+		
+	}
+}
