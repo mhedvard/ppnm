7a02e2821f259fcdcef22f160536c132510afe1a
Michelle <mhedvard@gmail.com>
2022-04-20 13:54:00 +0200

    Did eigenvalues part A

---
+++ homework/eigenvalues/A/main.cs
@@ -0,0 +1,54 @@
+using System; 
+using static System.Console; 
+using static System.Math; 
+
+class main{
+	static void Main(){
+		// Create real symetric matrix A
+		int n = 3; 
+		matrix A = new matrix(n,n);
+		var rnd = new Random(); 
+		for(int i =0; i<n; i++){
+			for(int j = 0; j <=i; j++){
+				A[i,j] = rnd.Next(10);
+				A[j,i] = A[i,j];
+			}
+		}
+		
+		A.print("Matrix used for the test");		
+		
+		// Find eigenvalues and vectors
+		vector eig; 
+		matrix V;
+		(eig,V) = jacobi.cyclic(A);
+
+
+		WriteLine(""); 
+		eig.print("Eigenvalues e =");
+
+		WriteLine("");
+		V.print("Orthogonal matrix of eigenvectors V = ");
+
+		var D = new matrix(eig);
+		WriteLine(""); 
+		D.print("Diagonal matrix with the corresponding eigenvalues D = ");
+
+		//Test
+		WriteLine("");
+		var test1 = V.T*A*V;
+		test1.print("Test: V^TAV=D:");
+
+
+		WriteLine("");
+		var test2 = V*D*V.T;
+		test2.print("Test: VDV^T=A:");
+
+		WriteLine("Test: V^TV=I:");
+		var test3 = V.T*V;
+		test3.print();
+
+		WriteLine($"");
+		var test4 = V*V.T;
+		test4.print("Test: VV^T=I:");
+	}
+}
