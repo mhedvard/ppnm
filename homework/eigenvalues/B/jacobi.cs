ceca785f09187fddf3ed53e91f978756f4f3b4ea
Michelle <mhedvard@gmail.com>
2022-04-22 10:46:56 +0200

    working in eig part B

---
+++ homework/eigenvalues/B/jacobi.cs
@@ -0,0 +1,80 @@
+using System; 
+using static System.Console;
+using static System.Math;
+
+public class jacobi{	
+	static void timesJ(matrix A, int p, int q, double theta){
+		double aip,aiq;
+		double c = Cos(theta);
+		double s = Sin(theta);
+		for(int i=0; i<A.size1; i++){
+			aip = A[i,p];
+			aiq = A[i,q];
+			A[i,p]= c*aip - s*aiq;
+			A[i,q]= s*aip + c*aiq;
+		}
+	}	
+
+	static void Jtimes(matrix A, int p, int q, double theta){
+		double aip, aiq;
+		double c = Cos(theta);
+		double s = Sin(theta);
+		for(int j=0; j<A.size2; j++){
+			aip = A[p,j];
+			aiq = A[q,j];
+			A[p,j]= c*aip + s*aiq;
+			A[q,j]= - s*aip + c*aiq;
+		}
+	}	
+
+	static public (vector,matrix) cyclic(matrix A){
+		matrix D = A.copy(); 
+		int n = D.size1;
+		var V = matrix.id(n); 
+	
+		
+		double diff = 1;
+		double diag0 = 0;
+		double diag1 = 0;
+		for(int i = 0; i<n; i++)
+			diag1 += Abs(D[i,i]);
+		/* //Alternative convert criterium (snall sum of aff diagonals)
+		double offDiag = 0; 
+		for(int i=0; i<A.size1; i++){
+			for(int j = 0; j< A.size2; j++){
+				if(i!=j)
+					offDiag+=Abs(D[i,j]);
+			}
+		}
+		*/
+		while(diff>1e-6){
+			for( int i = 0; i < n; i++){
+				for(int j=0; j < i; j++){
+					double theta = 0.5*Atan2(2*D[i,j],D[j,j]-D[i,i]);
+					Jtimes(D,i,j,-theta);
+					timesJ(D,i,j,theta);
+					timesJ(V,i,j,theta);
+				}
+			}
+		/* //Alternative convert criterium (snall sum of aff diagonals)
+			offDiag = 0; 
+			for(int i=0; i<A.size1; i++){
+				for(int j = 0; j< A.size2; j++){
+					if(i!=j)
+						offDiag+=Abs(D[i,j]);
+				}
+			}
+		*/
+			diag0 = diag1; 
+			diag1 = 0;
+			for(int i = 0; i<n; i++)
+				diag1 += Abs(D[i,i]); 
+			diff = Abs(diag1-diag0);
+					
+		}
+		// Convert matrix D to a vector with the eignevalues. 
+		vector eig; 
+		eig = D.diag();
+		return (eig,V);
+	} 
+}
---
+++ homework/eigenvalues/B/main.cs
@@ -0,0 +1,51 @@
+using System; 
+using static System.Console; 
+using static System.Math; 
+using System.IO;
+class main{
+	static void Main(){
+	// Investigate convergence of your energies with respect to and dr.
+		StreamWriter sw_dr = new StreamWriter("drConv.dat");
+		for(int N = 10; N<200; N+=5){
+			double rmax = 10; 
+			var H = hem(rmax,N);
+			// Diagonalize the matrix using your Jacobi routine
+			matrix V; 
+			vector eig; 
+		
+			(eig,V) = jacobi.cyclic(H);
+			sw_dr.WriteLine($"{rmax/(N+1)}  {eig[0]} {eig[1]} {eig[2]}	-0.5	-0.125	-0.055" );
+		}
+		sw_dr.Close();
+
+
+	// Investigate convergence of your energies with respect to rmax.
+		StreamWriter sw_rmax = new StreamWriter("rmaxConv.dat");
+		for(double rmax = 10; rmax<200; rmax+=5){
+			int N = 100; 
+			var H = hem(rmax,N);
+			// Diagonalize the matrix using your Jacobi routine
+			matrix V; 
+			vector eig; 
+		
+			(eig,V) = jacobi.cyclic(H);
+			sw_rmax.WriteLine($"{rmax}  {eig[0]} {eig[1]} {eig[2]}");
+		}
+		sw_rmax.Close();
+	}
+
+	static matrix hem(double rmax, int N){
+		// Boild the Hamiltoanian matrix
+		double dr = rmax/(N+1);
+		var r = new vector(N); 
+		var H = new matrix(N,N); 
+		for(int i=0;i<N;i++)
+			r[i]=dr*(i+1);
+		for(int i=0; i<N; i++){
+			H[i,i] = 1/dr/dr-1/r[i]; 
+			if(i>0)	H[i,i-1]=- 0.5/dr/dr; 
+			if(i<N-1) H[i,i+1]=-0.5/dr/dr;
+		} 
+		return H; 
+	}
+}
