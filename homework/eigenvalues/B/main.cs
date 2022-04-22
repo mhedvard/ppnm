bd83d3b50158a9f0ce667bc85f1b398d5444a88f
Michelle <mhedvard@gmail.com>
2022-04-22 14:30:34 +0200

    working on eig part B

--- homework/eigenvalues/B/main.cs
+++ homework/eigenvalues/B/main.cs
@@ -6,7 +6,7 @@ class main{
 	static void Main(){
 	// Investigate convergence of your energies with respect to and dr.
 		StreamWriter sw_dr = new StreamWriter("drConv.dat");
-		for(int npoints = 50; npoints<150; npoints+=5){
+		for(int npoints = 10; npoints<200; npoints+=10){
 			double rmax = 30;
 			double dr = rmax/(npoints+1);
  
@@ -20,36 +20,22 @@ class main{
 		}
 		sw_dr.Close();
 
-/*
+
 	// Investigate convergence of your energies with respect to rmax.
 		StreamWriter sw_rmax = new StreamWriter("rmaxConv.dat");
 		for(double rmax = 1; rmax<20; rmax+=2){
-			double dr = 0.2; 
-			var H = hem(rmax,dr);
+			double dr = 0.2;
+			int npoints = (int) (rmax/dr)-1; 
+			var H = hem(rmax,npoints);
 			// Diagonalize the matrix using your Jacobi routine
-			matrix V; 
-			vector eig; 
-		
-			(eig,V) = jacobi.cyclic(H);
+			(vector eig, matrix V) = jacobi.cyclic(H);
 			sw_rmax.WriteLine($"{rmax}  {eig[0]} {eig[1]} {eig[2]}	-0.5	-0.125	-0.055");
 		}
 		sw_rmax.Close();
-*/	
+
 	}
 
 	static matrix hem(double rmax, int npoints){
-		/* // Build the Hamiltoanian matrix
-		int N = (int) (rmax/dr)-1;
-		var r = new vector(N); 
-		var H = new matrix(N,N); 
-		for(int i=0;i<N;i++)
-			r[i]=dr*(i+1);
-		for(int i=0; i<N; i++){
-			H[i,i] = 1/dr/dr-1/r[i]; 
-			if(i>0)	H[i,i-1]=- 0.5/dr/dr; 
-			if(i<N-1) H[i,i+1]=-0.5/dr/dr;
-		}
-*/
 		double dr = rmax/(npoints+1);
 		vector r = new vector(npoints);
 		for(int i=0;i< npoints;i++)
