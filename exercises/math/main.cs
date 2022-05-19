a14ada1ee5752603443974a198c4a06084fd71fb
Michelle <mhedvard@gmail.com>
2022-05-19 15:50:07 +0200

    checked exercies and small corrections

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -6,13 +6,17 @@ class math{
 	static void Main(){
 		double sqrt2=Sqrt(2.0); 
 		Write($"sqrt(2) = {sqrt2}\n");
-		Write($"sqrt2*sqrt2 = {sqrt2*sqrt2} (should be equal 2)\n");
+		Write($"sqrt(2)*sqrt(2) = {sqrt2*sqrt2} (should be equal 2)\n");
+		WriteLine(""); 
 
 		double epi=Exp(PI); 
 		Write($"e^pi = {epi}\n");
 		Write($"ln(e^pi) = {Log(epi)} (should be equal pi)\n");
+		WriteLine("");
 
 		double pie=Pow(PI,E); 
 		Write($"pi^e = {pie}\n");
+		Write($"ln(pi^e)/ln(pi) = {Log(pie)/Log(PI)} (should be equal e={E})\n");
+		WriteLine("");
 	}
 }
