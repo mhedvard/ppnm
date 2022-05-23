98b6e59ce7b8a3a985b04cd09df8a5503b5a9ab5
Michelle <mhedvard@gmail.com>
2022-05-23 13:22:55 +0200

    Updated main.cs to get better Out.txt

--- homework/generic_list/B/main.cs
+++ homework/generic_list/B/main.cs
@@ -15,7 +15,7 @@ class main{
 				numbers[i] = double.Parse(words[i]); 
 			list.push(numbers); 
 		}
-		
+		WriteLine("List contains:");
 		for(int i=0; i<list.size;i++){
 			var numbers = list.data[i];
 			foreach(var number in numbers)
@@ -28,8 +28,8 @@ class main{
 		int i_remove = 1; // Index of list to remove
 		list.remove(i_remove); 
 
-		WriteLine($"List of index {i_remove} is remowed");
-		
+		WriteLine($"Values for index {i_remove} is remowed from list");
+		WriteLine("List contains:");
 		for(int i=0; i<list.size; i++){
 			var numbers = list.data[i];
 			foreach(var number in numbers)
