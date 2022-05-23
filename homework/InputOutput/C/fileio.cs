2de5c7bc54ae344d6a253c26391154a84cefaedc
Michelle <mhedvard@gmail.com>
2022-05-23 11:39:50 +0200

    Update Makefile

--- homework/InputOutput/C/fileio.cs
+++ homework/InputOutput/C/fileio.cs
@@ -4,26 +4,26 @@ using static System.Math;
 
 class main{
 	public static int Main(string[] args){
-	        string infile=null, outfile=null;
+		string infile=null, outfile=null;
 		foreach(var arg in args){
 			var words = arg.Split(':');
 
 			if(words[0] == "-input") 
 				infile = words[1];
-	                else if(words[0] == "-output") 
+			else if(words[0] == "-output") 
 				outfile = words[1];
-	                else { 
+			else{ 
 				Error.WriteLine("wrong argument"); 
 				return 1; 
-				}
-	                }
+			}
+		}
 
-	        var instream = new System.IO.StreamReader(infile);
-	        var outstream = new System.IO.StreamWriter(outfile);
-	        for(string line = instream.ReadLine(); line!=null; line=instream.ReadLine()){
-	                double x = double.Parse(line);
-	                outstream.WriteLine($"{x} {Sin(x)} {Cos(x)}");
-	        }
+		var instream = new System.IO.StreamReader(infile);
+		var outstream = new System.IO.StreamWriter(outfile);
+		for(string line = instream.ReadLine(); line!=null; line=instream.ReadLine()){
+			double x = double.Parse(line);
+			outstream.WriteLine($"{x} {Sin(x)} {Cos(x)}");
+		}
         instream.Close();
         outstream.Close();
 	return 0;
