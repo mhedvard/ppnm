7355d8522725664875486974e59b9c8143b04851
Michelle <mhedvard@gmail.com>
2022-05-23 15:15:01 +0200

    Updated Makefile and change name to main

--- homework/plots/A/main.cs
+++ homework/plots/A/main.cs
@@ -9,7 +9,7 @@ static class main{
 
 		double[] a={0.254829592,-0.284496736,1.421413741,-1.453152027,1.061405429};
 		double t=1/(1 + 0.3275911 * x);
-		double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));/* the right thing */
+		double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));
 		return 1-sum* Exp(-x * x);
 	} 
 
