d43f144acf5c244ec6424f7716fc4ed2c19a6297
Michelle <mhedvard@gmail.com>
2022-05-24 11:30:24 +0200

    did erf plot

--- exercises/integration/main.cs
+++ exercises/integration/main.cs
@@ -1,13 +1,15 @@
+using System; 
 using static System.Math;
 using static System.Console;
-using System;
-using static integrate;
+using System.IO;
+
 
 class main{
 	static void Main(){
-		System.Func<double,double> f = delegate(double x){
+		Func<double,double> f = delegate(double x){
 			return Log(x)/Sqrt(x);
 		};
+		
 
 		double a = 0; 
 		double b = 1; 
@@ -16,5 +18,18 @@ class main{
 
 		WriteLine($"The result for int(ln(x)/sqrt(x))dx from 0 to 1: {r}.");
 		WriteLine("The exact value is: -4");
+	
+		// Error function //
+		StreamWriter erfdata = new StreamWriter("erf.dat");
+		for(double x = -3; x<=3; x+=0.1){
+			erfdata.WriteLine($"{x}	{erf(x)}"); 
+		}
+		erfdata.Close();
+	}
+	public static double erf(double z){
+		Func<double, double> i = delegate(double x){
+		return Exp(-x*x);
+		};
+		return 2/Sqrt(PI)*integrate.quad(i,0,z); 
 	}
 }
