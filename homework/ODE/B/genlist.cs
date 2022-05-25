acf9a67773f700e394e711813b8e5222a830f609
Michelle <mhedvard@gmail.com>
2022-05-25 10:12:06 +0200

    Updated genericlist to new version

--- homework/ODE/B/genlist.cs
+++ homework/ODE/B/genlist.cs
@@ -1,13 +1,33 @@
 public class genlist<T>{
 	public T[] data; 
-	public int size {get{return data.Length;}} // property
-	public genlist(){ data = new T[0];} // constructor
+	public int size, capacity;
+
+	public genlist(){ 
+		size = 0; 
+		capacity = 8; 
+		data = new T[capacity];
+	} 
+
 	public void push(T item) {
-		T[] newdata = new T[size+1]; 
-		for(int i=0;i<size;i++)
-			newdata[i] = data[i]; 
-		
-		newdata[size] = item;
+		if(size == capacity){
+			T[] newdata = new T[capacity*=2];
+			for (int i=0; i<size; i++)
+				newdata[i] = data[i];
+			data = newdata; 
+		}
+		data[size] =item;
+		size++;
+	}
+
+	public void remove(int i){
+		T[] newdata = new T[capacity]; 
+		for(int k=0; k<size; k++){	
+			if(k<i)
+				newdata[k] = data[k];
+			else if(k>i)
+				newdata[k-1]=data[k];
+		}
 		data = newdata;
+		size--;
 	}
 }
