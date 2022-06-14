6fa1ecb7bf91afac6419ea216d1abf2693293c6c
Michelle <mhedvard@gmail.com>
2022-06-14 16:11:09 +0200

    Runges phen

---
+++ exam/RungesPhen/cspline.cs
@@ -0,0 +1,98 @@
+using System; 
+using static System.Console;
+using static System.Math;
+
+public class cspline {
+	double[] x,y,b,c,d;
+	public cspline(double[] xs, double[] ys){
+		x = xs; 
+		y = ys;
+		int n = x.Length;
+
+		b = new double[n];
+		c = new double[n-1];
+		d = new double[n-1];
+		
+		var h = new  double[n-1];
+		var p = new  double[n-1];
+		
+		var B = new  double[n];
+		var D = new  double[n];
+		var Q = new  double[n-1];
+
+		var Dt = new double[n];
+		var Bt = new double[n];
+		
+
+		for(int i = 0; i < n-1; i++){
+			h[i] = x[i+1]-x[i];
+			p[i] = (y[i+1]-y[i])/h[i]; //dydx
+		}	
+		
+		D[0] = 2; D[n-1]=2; Q[0] = 1; B[0] = 3*p[0]; B[n-1]=3*p[n-2];
+		for(int i = 0; i < n-2; i++){
+			D[i+1] = 2*h[i]/h[i+1]+2;
+			Q[i+1] = h[i]/h[i+1];
+			B[i+1] = 3*(p[i]+p[i+1]*h[i]/h[i+1]);
+		}
+
+		
+		Dt[0] = D[0]; Bt[0] =B[0];
+		for(int i = 1; i < n; i++){
+			Dt[i] = D[i]-Q[i-1]/D[i-1];
+			Bt[i] = B[i] - Bt[i-1]/Dt[i-1];
+		}
+
+		b[n-1] = Bt[n-1]/Dt[n-1];
+		for(int i=n-2; i>=0; i--)
+			b[i]=(Bt[i]-Q[i]*b[i+1])/Dt[i];
+
+
+		for(int i=0; i<n-1; i++){
+			c[i]=(-2*b[i]-b[i+1]+3*p[i])/h[i];
+			d[i]=(b[i]+b[i+1]-2*p[i])/Pow(h[i],2);
+		}
+	}
+	
+	public double spline(double z){
+		int i = binsearch(z);
+		return y[i] + b[i]*(z-x[i])+c[i]*Pow((z-x[i]),2)+d[i]*Pow((z-x[i]),3);
+	}
+
+
+	public double derivative(double z){
+		int i = binsearch(z); 
+		return b[i] +2 * c[i] *(z-x[i]) + 3 * d[i] * Pow(z-x[i],2);	
+	}
+
+
+	public double integral(double z){
+		int j = binsearch(z); 
+		double val = 0; 
+		for(int i = 0; i < j; i++)
+			val +=  partint(i,x[i+1]);
+		val +=  partint(j,z);
+		return val;
+	}
+
+	int binsearch(double z){
+		/*Locate the idenx i for which x_i<=z<=x_(i+1)*/
+		int i = 0;
+		int j = x.Length-1;
+		
+		if(x[0] > z || x[j] < z)
+			throw new Exception("binsearch: bad z");
+ 
+		while(j - i > 1){
+			int m = (i+j)/2; 
+			if(z > x[m]) i = m; 
+			else j = m; 
+		}
+		return i; 
+	}
+	double partint(int i, double z){
+		return (z-x[i])/12*(12*y[i] + 6*b[i]*(z-x[i]) + 4*c[i]*Pow(z-x[i],2) + 3*d[i] *(Pow(z,3)-3*x[i]*Pow(z,2) + 3*z*Pow(x[i],2) -Pow(x[i],3)));
+	}
+
+}
+
--- exam/RungesPhen/main.cs
+++ exam/RungesPhen/main.cs
@@ -5,48 +5,20 @@ using System.IO;
 
 class main{
 	static void Main(){
-		////////////////////////////////////////////////
-		// Compare wiggles for cubic spline and Akima //
-		////////////////////////////////////////////////
-		double[] x = {0,1,2,3,4,5,6,7,8,9,10}; 
-		double[] y = {-1,-1,-1,1,1,1,1,1,0,0,0};
-		
-		var subs = new akima(x,y);	
-		var cs = new cspline(x,y); 
-		
-		StreamWriter sw = new StreamWriter("testWiggle.dat"); 
-		for(int i = 0; i < x.Length; i++)
-			sw.WriteLine($"{x[i]}	{y[i]}"); 
-		sw.WriteLine("");
-		sw.WriteLine("");		
-
-		for(double xp = x[0]; xp < x[x.Length-1]; xp+=0.1){
-			sw.WriteLine($"{xp}	{subs.getS(xp)}");
-		}
-		sw.WriteLine("");
-		sw.WriteLine("");		
-
-		for(double xp = x[0]; xp < x[x.Length-1]; xp+=0.1){
-			sw.WriteLine($"{xp}	{cs.spline(xp)}");
-		}
-		sw.Close(); 
-
-
-
 		////////////////////////////////////////////////////
 		// Rounge's phenomenom for cubic spline and Akima //
 		////////////////////////////////////////////////////
 		double[] x = {-5,-4,-3,-2,-1,1,2,3,4,5}; 
 		double[] y = new double[x.Length];
 
-		for(int i = 0; i < x.length; i++){
+		for(int i = 0; i < x.Length; i++){
 			y[i] = 1/(1+25*x[i]*x[i]);
 		}
 		
 		var subs = new akima(x,y);	
 		var cs = new cspline(x,y); 
 		
-		var sw = new StreamWriter("testRungesPhen.dat"); 
+		var sw = new StreamWriter("RungesPhen.dat"); 
 		for(int i = 0; i < x.Length; i++)
 			sw.WriteLine($"{x[i]}	{y[i]}"); 
 		sw.WriteLine("");
