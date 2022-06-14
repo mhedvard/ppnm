af8decb218ce8e0ed6b14891a4e787149b624b4d
Michelle <mhedvard@gmail.com>
2022-06-14 12:03:16 +0200

    included cspline

--- exam/main.cs
+++ exam/main.cs
@@ -1,14 +1,15 @@
 using System; 
 using static System.Console; 
 using static System.Math; 
-//using akima; 
+
 
 class main{
 	static void Main(){
 		double[] x = {0,1,2,3,4,5}; 
 		double[] y = {-1,-1,-1,1,1,1};
 		
-		var subs = new akima(x,y);		
+		var subs = new akima(x,y);	
+		var cs = new cspline(x,y); 
 
 		for(int i = 0; i < x.Length; i++)
 			WriteLine($"{x[i]}	{y[i]}"); 
@@ -17,6 +18,12 @@ class main{
 
 		for(double xp = x[0]; xp < x[x.Length-1]; xp+=0.1){
 			WriteLine($"{xp}	{subs.getS(xp)}");
+		}
+		WriteLine("");
+		WriteLine("");		
+
+		for(double xp = x[0]; xp < x[x.Length-1]; xp+=0.1){
+			WriteLine($"{xp}	{cs.spline(xp)}");
 		}
 	}
 
