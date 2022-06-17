4d1ff5bf322ec84ab358e29e4430e0ab26c4dc5c
Michelle <mhedvard@gmail.com>
2022-06-17 14:47:54 +0200

    test plot

---
+++ exam/ImplementAkima/main.cs
@@ -0,0 +1,27 @@
+using System; 
+using static System.Console; 
+using static System.Math; 
+using System.IO; 
+
+class main{
+	static void Main(){
+		double[] x = {0,1,2,3,4,5,6,7,8,9,10};
+		double[] y = new double[x.Length];
+		for(int i = 0; i < x.Length; i++)
+			y[i] = Sin(x[i]);
+		
+		var subs = new akima(x,y); 
+		
+		StreamWriter sw = new StreamWriter("testAkima.dat"); 
+		for(int i = 0; i < x.Length; i++)
+			sw.WriteLine($"{x[i]}	{y[i]}"); 
+		sw.WriteLine("");
+		sw.WriteLine("");		
+
+		for(double xp = x[0]; xp < x[x.Length-1]; xp+=0.1){
+			sw.WriteLine($"{xp}	{subs.getS(xp)}	{subs.derivative(xp)}	{subs.integral(xp)}");
+		}
+		sw.Close(); 
+	}
+}
+
