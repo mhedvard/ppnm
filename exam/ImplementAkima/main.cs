4cacc2894e93b08b3a0fd6866f722e26e339c431
Michelle <mhedvard@gmail.com>
2022-06-17 16:10:09 +0200

    added describtion to Out.txt

--- exam/ImplementAkima/main.cs
+++ exam/ImplementAkima/main.cs
@@ -5,11 +5,28 @@ using System.IO;
 
 class main{
 	static void Main(){
-		double[] x = {0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5};
-		double[] y = new double[x.Length];
-		for(int i = 0; i < x.Length; i++)
+		// Data points 
+		int np = 20;
+		double[] x = new double[np];
+		double[] y = new double[np];
+		for(int i = 0; i < np; i++){
+			x[i] = i*0.5;
 			y[i] = Sin(x[i]);
+		}
 		
+		// Describtion of what is done to wtitten out in file
+		WriteLine("The Akima subspline is used to interpolate the function sin(x) using the datapoints:");
+		WriteLine(); 
+		WriteLine("x	y");
+		for(int i = 0; i<np; i++) 
+			WriteLine($"{x[i]}	{y[i]}");
+		WriteLine();
+		WriteLine("From the interpolation the derivative and intergral (from 0 to x) is determind.");
+		WriteLine("Remember that for the Akima-subsline the continuity of the second derevative is relaxed,");
+		WriteLine("to reduce wiggeling. Therefor the first derivative is not necessarily a smooth curve.");
+
+
+		// Akima subspline +  derevertive and integral 
 		var subs = new akima(x,y); 
 		
 		StreamWriter sw = new StreamWriter("testAkima.dat"); 
